773c5442d8474d255d6e806a492da294fd22f6b6
Zach Ahn <zach.ahn@gmail.com>
2014-09-27 21:06:21 -0400

    set test database to in-memory

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,10 @@ Rails.backtrace_cleaner.remove_silencers!
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
+# Silence migrations and load schema to in-memory database
+ActiveRecord::Migration.verbose = false
+load "#{Rails.root}/db/schema.rb"
+
 # Load fixtures from the engine
 if ActiveSupport::TestCase.method_defined?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
